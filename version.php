b738d624b8023f16c3c04edf6255c1805fc2ef62
Maskim Burnin <Maksim.Burnin@gmail.com>
2019-12-19 13:57:26 +0500

    perpage to 30

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'availability_examus';
-$plugin->version = 2019121901;
+$plugin->version = 2019121902;
 $plugin->release = 'v3.1-r4';
 $plugin->requires = 2016052300;
 $plugin->maturity = MATURITY_STABLE;
