d57bb175b00a2d729970539df8f998a588890e66
shofel <vistomin@examus.net>
2020-08-11 23:18:54 +0500

    common.php: Split the function `reset_entry` into two functions, and also
    - add some phpdocs
    - extract more functions: db_get_entries, create_new_entry
    - extract table name as a constant. What is a common practice?
    - delete_empty_entries: $cmdid checked for emptiness, so default value
      is not needed.

--- classes/common.php
+++ classes/common.php
@@ -4,62 +4,82 @@ use \stdClass;
 defined('MOODLE_INTERNAL') || die();
 
 class common {
-    public static function reset_entry($conditions, $force = false){
+    /**
+     * Name of the table used by the plugin.
+     */
+    public const TABLE = 'availability_examus';
+
+    /**
+     * Get entries from database.
+     */
+    private static function db_get_entries($where) {
         global $DB;
-
-        $oldentry = $DB->get_record('availability_examus', $conditions);
-
-        $not_inited = $oldentry && $oldentry->status == 'Not inited';
-
-        if ($oldentry && (!$not_inited || $force)) {
-            $entries = $DB->get_records('availability_examus', [
-                'userid' => $oldentry->userid,
-                'courseid' => $oldentry->courseid,
-                'cmid' => $oldentry->cmid,
-                'status' => 'Not inited'
-            ]);
-
-            if (count($entries) == 0 || $force) {
-                if($force){
-                    foreach($entries as $old){
-                        $old->status = "Force reset";
-                        $DB->update_record('availability_examus', $old);
-                    }
-                }
-
-                $timenow = time();
-                $entry = new stdClass();
-                $entry->userid = $oldentry->userid;
-                $entry->courseid = $oldentry->courseid;
-                $entry->cmid = $oldentry->cmid;
-                $entry->accesscode = md5(uniqid(rand(), 1));
-                $entry->status = 'Not inited';
-                $entry->timecreated = $timenow;
-                $entry->timemodified = $timenow;
-
-                $entry->id = $DB->insert_record('availability_examus', $entry);
-
-                return $entry;
-            } else {
-                return false;
-            }
-        }
+        return $DB->get_records(self::TABLE, $where);
     }
 
-    public static function delete_empty_entries($userid, $courseid, $cmid = null){
+    /**
+     * Create new entry with given `userid`, `courseid`, and `cmid`.
+     */
+    private static function create_new_entry($props) {
+        $timenow = time();
+        $entry = new stdClass();
+
+        $entry->userid = $props.userid;
+        $entry->courseid = $props.courseid;
+        $entry->cmid = $props.cmid;
+        $entry->accesscode = md5(uniqid(rand(), 1));
+        $entry->status = 'Not inited';
+        $entry->timecreated = $timenow;
+        $entry->timemodified = $timenow;
+
+        $entry->id = $DB->insert_record(self::$table, $entry);
+
+        return $entry;
+    }
+
+    /**
+     * Reset entry even if there are some not inited entries.
+     */
+    private static function reset_entry_forcefully($props) {
+        $entries = self::db_get_entries(['status' => 'Not inited']);
+
+        foreach ($entries as $x) {
+            global $DB;
+            $x->status = "Force reset";
+            $DB->update_record(self::TABLE, $x);
+        };
+
+        return self::create_new_entry($props);
+    }
+
+    /**
+     * Reset entry only when there are no not inited entries.
+     */
+    private static function reset_entry_gently($props) {
+        $some_not_inited = count(self::db_get_entries(['status' => 'Not inited']));
+
+        if ($some_not_inited) { return false; }
+        else { return self::create_new_entry($props); }
+    }
+
+    public static function reset_entry($props, $force = false){
+        $f = $force ? self::reset_entry_forcefully : self::reset_entry_gently;
+
+        return $f($props);
+    }
+
+    public static function delete_empty_entries($userid, $courseid, $cmid) {
         global $DB;
 
         $condition = [
-          'userid' => $userid,
-          'courseid' => $courseid,
-          'status' => 'Not inited'
+            'userid' => $userid,
+            'courseid' => $courseid,
+            'status' => 'Not inited'
         ];
 
-        if(!empty($cmid)) {
-            $condition['cmid'] = $cmid;
-        }
+        if (!empty($cmid)) { $condition['cmid'] = $cmid; }
 
-        $DB->delete_records('availability_examus', $condition);
+        $DB->delete_records(self::TABLE, $condition);
     }
 
 
