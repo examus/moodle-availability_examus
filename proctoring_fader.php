003fa0d1f19b500ed1a93b9f8910d586b44369d3
shofel <vistomin@examus.net>
2020-08-13 17:17:57 +0500

    examus fader: extract `examus_fader()`; replace queue with a promise.

--- proctoring_fader.php
+++ proctoring_fader.php
@@ -17,8 +17,15 @@
 
 var str_awaiting_proctoring = <?= json_encode(get_string('fader_awaiting_proctoring', 'availability_examus')) ?>;
 var str_instructions = <?= json_encode(get_string('fader_instructions', 'availability_examus')) ?>;
-//msg queue, inited ASAP, so we don't miss anything
-var examus_q = [];
+let fader_html = str_awaiting_proctoring + str_instructions
+
+let {sessionStorage, location} = window
+
+let TAG = 'proctoring fader'
+
+let key = 'examus-client-origin'
+
+let expected_data = 'proctoringReady_n6EY'
 
 /*
  * Origin of sender, that is of the proctoring application.
@@ -26,11 +33,6 @@ var examus_q = [];
  * then we've got a previously known value.
  */
 
-let {sessionStorage, location} = window
-
-let TAG = 'proctoring fader'
-
-let key = 'examus-client-origin'
 let from_storage = () => sessionStorage.get(key)
 let to_storage = x => sessionStorage.set(key, x)
 let from_url = () =>
@@ -46,37 +48,69 @@ if (!from_storage()) to_storage(from_url())
 
 let expected_origin = from_storage()
 
-if (!expected_orgin) {
+if (!expected_origin) {
   console.error(TAG, 'missing `expected_origin`')
 }
 
-      window.addEventListener("message", function(e){
-        console.debug(TAG, 'got some message', e.origin, expected_origin);
-        if(e.origin === expected_origin){
-          examus_q.push(e.data); console.debug(TAG, 'got proved message', e.data);
-        }
-        check();
-      });
+/**
+ * Promise, which resolves when got a message proving the page is being proctored.
+ * TODO postpone the effect
+ */
+let proved = new Promise(resolve => {
+  let f = e => {
+    console.debug(TAG, 'got some message', e.origin, expected_origin)
 
-      var examusFader;
-      window.addEventListener("DOMContentLoaded", function(){
-        console.log("loaded");
-        examusFader = document.createElement("DIV");
-        examusFader.innerHTML = str_awaiting_proctoring + str_instructions;
-        examusFader.style="position: fixed; z-index: 1000; font-size: 2em; width: 100%; height: 100%; background: #fff; top: 0; left: 0;text-align: center;display: flex;justify-content: center;align-content: center;flex-direction: column;";
-        document.body.appendChild(examusFader);
-        if(!check()){
-          if(window.parent && window.parent != window){
-            window.parent.postMessage('proctoringRequest', expected_origin);
-          }
-        }
-      });
-      function check(){
-        if(examus_q && examus_q[0]){
-          unlock();
-          return true;
-        }
-      }
-      function unlock(){ if(examusFader) examusFader.remove(); examusFader = null; }
-    })();
+    if (e.origin === expected_origin &&
+        e.data === expected_data
+    ) {
+      resolve()
+      console.debug(TAG, 'got proved message', e.data)
+      window.removeEventListener('message', f)
+    }
+  }
+
+  window.addEventListener("message", f)
+})
+
+/**
+ * Prepare the element to cover quiz contents.
+ */
+let examus_fader = () => {
+  let x = document.createElement("div")
+
+  x.innerHTML = fader_html
+
+  let style = {
+    position: 'fixed',
+    zIndex: 1000,
+    font-size: 2em,
+    width: 100%,
+    height: 100%,
+    background: #fff,
+    top: 0,
+    left: 0,
+    text-align: center,
+    display: flex,
+    justify-content: center,
+    align-content: center,
+    flex-direction: column
+  }
+
+  Object.assign(x.style, style)
+
+  return x
+}
+
+  window.addEventListener("DOMContentLoaded", function() {
+    let el = examus_fader()
+
+    document.body.appendChild(el)
+
+    proved.then(() => el.remove())
+
+    /* Most of the time this action is meaningless,
+     * at the same time it's always harmless. */
+    window.parent.postMessage('proctoringRequest', expected_origin);
+  });
+})();
 </script>
