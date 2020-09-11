5516f42f35aa97e1a9d40ee9425a6d7c53040892
Maskim Burnin <Maksim.Burnin@gmail.com>
2020-09-11 19:47:01 +0500

    Version bump

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'availability_examus';
-$plugin->version = 2020090701;
+$plugin->version = 2020091101;
 $plugin->release = 'v0.8';
 $plugin->requires = 2017051500;
 $plugin->maturity = MATURITY_STABLE;
